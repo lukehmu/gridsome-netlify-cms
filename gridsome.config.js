2ef38ce84755f6eb086677d9aa2afca5aa36dc7b
Luke <lukehmu@gmail.com>
2019-09-25 21:17:36 +0100

    integrating netlify cms

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,7 +8,10 @@ module.exports = {
   siteName: 'Lukehmu',
   plugins: [
     {
-      use: `gridsome-plugin-netlify-cms`
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: "/admin"
+      }
     }
   ]
 }
